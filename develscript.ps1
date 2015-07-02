ffbd69aebd2e1139c0cf4a2390fbdbaae6d31758
Trond Hindenes <trond@hindenes.com>
2015-07-02 22:11:23 +0200

    mere devel

--- develscript.ps1
+++ develscript.ps1
@@ -1,1 +1,2 @@
-write-host "Hello"
+$guid = [system.guid]::NewGuid().ToString()
+set-content -path "D:\trond.hindenes\Documents\Projects-VS2013\swashtest\$guid.txt" -Value $MyInvocation.MyCommand.Path.ToString()

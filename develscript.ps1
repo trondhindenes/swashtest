728af085f58f59aa14bcd4e5985c77e0cee25fcb
Trond Hindenes <trond@hindenes.com>
2015-07-02 22:53:41 +0200

    throw

--- develscript.ps1
+++ develscript.ps1
@@ -1,3 +1,4 @@
 $guid = [system.guid]::NewGuid().ToString()
 Set-Content -Path "D:\trond.hindenes\Documents\Projects-VS2013\swashtest\$guid.txt" -Value ((get-date).ToString()) -Force
 Add-Content -Path "D:\trond.hindenes\Documents\Projects-VS2013\swashtest\$guid.txt" -Value "this is devel"
+throw "someerror"

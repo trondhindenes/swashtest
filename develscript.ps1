103a23418d7b74d81385225ec43d024f0c91278c
Trond Hindenes <trond@hindenes.com>
2015-07-02 22:30:49 +0200

    This is devel

--- develscript.ps1
+++ develscript.ps1
@@ -1,2 +1,3 @@
 $guid = [system.guid]::NewGuid().ToString()
 Set-Content -Path "D:\trond.hindenes\Documents\Projects-VS2013\swashtest\$guid.txt" -Value ((get-date).ToString()) -Force
+Add-Content -Path "D:\trond.hindenes\Documents\Projects-VS2013\swashtest\$guid.txt" -Value "this is devel"

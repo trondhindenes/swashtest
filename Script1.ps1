4cfbad7e48d2fce92cd76783321d927f10fc0e9d
trondhindenes <trond@hindenes.com>
2015-07-09 15:20:08 +0200

    Update Script1.ps1

--- Script1.ps1
+++ Script1.ps1
@@ -1,2 +1,3 @@
 $guid = [system.guid]::NewGuid().ToString()
+write-host "test"
 Set-Content -Path "D:\trond.hindenes\Documents\Projects-VS2013\swashtest\$guid.txt" -Value ((get-date).ToString()) -Force
